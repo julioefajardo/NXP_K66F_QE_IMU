002ce6586908365b3fc901608d3d02fc0ffbad65
julioefajardo <julioefajardo@gmail.com>
2017-10-04 15:33:12 -0600

    Tabs fixed

--- K66F_Rover/main.c
+++ K66F_Rover/main.c
@@ -68,111 +68,111 @@ int16_t omega_b = 0;
 
 int main(void){
 		
-	// Left Motor PID Definitions
-	Left_PID.Kp = PID_L_KP;        /* Proporcional */
-	Left_PID.Ki = PID_L_KI;        /* Integral */
-	Left_PID.Kd = PID_L_KD;        /* Derivative */
+  // Left Motor PID Definitions
+  Left_PID.Kp = PID_L_KP;        /* Proporcional */
+  Left_PID.Ki = PID_L_KI;        /* Integral */
+  Left_PID.Kd = PID_L_KD;        /* Derivative */
     
-	// Right Motor PID Definitions
-	Right_PID.Kp = PID_R_KP;        /* Proporcional */
-	Right_PID.Ki = PID_R_KI;        /* Integral */
-	Right_PID.Kd = PID_R_KD;        /* Derivative */
+  // Right Motor PID Definitions
+  Right_PID.Kp = PID_R_KP;        /* Proporcional */
+  Right_PID.Ki = PID_R_KI;        /* Integral */
+  Right_PID.Kd = PID_R_KD;        /* Derivative */
 	
 	// Hardware Initialization
-	LEDs_Init();
-	QD_Init(&QD_L,1);
-	QD_Init(&QD_R,2);
-	Motor_Init(1);
-	Motor_Init(2);
-	UART_Init(115200);
-	PIT_Init(PIT_FREQUENCY);
-	SysTick_Config(SystemCoreClock/20);
+  LEDs_Init();
+  QD_Init(&QD_L,1);
+  QD_Init(&QD_R,2);
+  Motor_Init(1);
+  Motor_Init(2);
+  UART_Init(115200);
+  PIT_Init(PIT_FREQUENCY);
+  SysTick_Config(SystemCoreClock/20);
 	
-	// Quadrature Decoding Process Initialization
-	QD_Process(&QD_L,1);
-	QD_Process(&QD_R,2);
+  // Quadrature Decoding Process Initialization
+  QD_Process(&QD_L,1);
+  QD_Process(&QD_R,2);
 	
-	// PID Initialization
-	arm_pid_init_f32(&Left_PID,1);
-	arm_pid_init_f32(&Right_PID,1);
+  // PID Initialization
+  arm_pid_init_f32(&Left_PID,1);
+  arm_pid_init_f32(&Right_PID,1);
 	
-	while(1){
-		if(data_ready){
-			omega_a = atoi(num1);
-			omega_b = atoi(num2);
-			Left_SP = omega_a/(100.0f);
-			Right_SP = omega_b/(100.0f);
-			if (omega_a > 123) LED_On(0);
-			else LED_Off(0);
-			if (omega_b > 123) LED_On(2);
-			else LED_Off(2);
-			data_ready = 0;
-		} 
-	}
+  while(1){
+	  if(data_ready){
+		  omega_a = atoi(num1);
+		  omega_b = atoi(num2);
+		  Left_SP = omega_a/(100.0f);
+		  Right_SP = omega_b/(100.0f);
+		  if (omega_a > 123) LED_On(0);
+		  else LED_Off(0);
+		  if (omega_b > 123) LED_On(2);
+		  else LED_Off(2);
+		  data_ready = 0;
+	  } 
+  }
 }
 
 
 // Debugging through UART 
 void SysTick_Handler(void){
-	LED_Toggle(1);
-	sprintf(string,"%.2f,\t%.2f,\t%.2f,\t%.2f\r",Left_SP,QD_L.omega,Right_SP,QD_R.omega);
-	//UART_PutString(string);
+  LED_Toggle(1);
+  sprintf(string,"%.2f,\t%.2f,\t%.2f,\t%.2f\r",Left_SP,QD_L.omega,Right_SP,QD_R.omega);
+  //UART_PutString(string);
 }
 
 
 // Angular Velocity Controller
 void PIT0_IRQHandler(void){
-	PIT->CHANNEL[0].TFLG |= PIT_TFLG_TIF_MASK;																										// Clear Flag
-	QD_Process(&QD_L,1);																																					// Quadrature Decoder Processing
-	QD_Process(&QD_R,2);
-	Left_error = Left_SP - QD_L.omega;																														// Error
-	Right_error = Right_SP - QD_R.omega;
-	L_Motor = ((Left_SP>=-0.01f)&&(Left_SP<=0.01f))?0.0f:arm_pid_f32(&Left_PID, Left_error);			// PID Output
-	R_Motor = ((Right_SP>=-0.01f)&&(Right_SP<=0.01f))?0.0f:arm_pid_f32(&Right_PID, Right_error);
-	L_Motor = Power_Verification(&L_Motor);																												// Power Verification (Between -1.0f and 1.0f)
-	R_Motor = Power_Verification(&R_Motor);
-	Motor_Set(&L_Motor,1);																																				// Set PWM to Motor Driver
-	Motor_Set(&R_Motor,2);
+  PIT->CHANNEL[0].TFLG |= PIT_TFLG_TIF_MASK;																										// Clear Flag
+  QD_Process(&QD_L,1);																																					// Quadrature Decoder Processing
+  QD_Process(&QD_R,2);
+  Left_error = Left_SP - QD_L.omega;																														// Error
+  Right_error = Right_SP - QD_R.omega;
+  L_Motor = ((Left_SP>=-0.01f)&&(Left_SP<=0.01f))?0.0f:arm_pid_f32(&Left_PID, Left_error);			// PID Output
+  R_Motor = ((Right_SP>=-0.01f)&&(Right_SP<=0.01f))?0.0f:arm_pid_f32(&Right_PID, Right_error);
+  L_Motor = Power_Verification(&L_Motor);																												// Power Verification (Between -1.0f and 1.0f)
+  R_Motor = Power_Verification(&R_Motor);
+  Motor_Set(&L_Motor,1);																																				// Set PWM to Motor Driver
+  Motor_Set(&R_Motor,2);
 }
 
 // UART Data Parser (Parse data from ROSSERIAL)
 void UART0_RX_TX_IRQHandler(void){
-	volatile uint8_t data ;
-	(void)UART0->S1;
-	data = UART0->D;
-	switch(num){
-		case 0:{
-			data_ready = 0;
-			if(data != ','){
-				if((data>='0')&&(data<='9')){
-					num1[i] = data;
-					i++;
-				} else {
-					if(data=='-'){
-						num1[i] = data;
-						i++;
-					}
-				}
-			} else{
-				num1[i] = '\0';
-				num = 1;
-				i = 0;
-			}
-		} break;
-		case 1:{
-			data_ready = 0;
-			if(data != '*'){
-				num2[i] = data;
-				i++;
-			} else{
-				num2[i] = '\0';
-				num = 2;
-				i = 0;
-			}
-		} break;
-		case 2:{
-			data_ready = 1;
-			num = 0;
-		} break;
-	}
+  volatile uint8_t data ;
+  (void)UART0->S1;
+  data = UART0->D;
+  switch(num){
+	  case 0:{
+		  data_ready = 0;
+		  if(data != ','){
+			  if((data>='0')&&(data<='9')){
+				  num1[i] = data;
+				  i++;
+			  } else {
+				  if(data=='-'){
+					  num1[i] = data;
+					  i++;
+				  }
+			  }
+		  } else{
+			  num1[i] = '\0';
+			  num = 1;
+			  i = 0;
+		  }
+	  } break;
+	  case 1:{
+		  data_ready = 0;
+		  if(data != '*'){
+			  num2[i] = data;
+			  i++;
+		  } else{
+			  num2[i] = '\0';
+			  num = 2;
+			  i = 0;
+		  }
+	  } break;
+	  case 2:{
+		  data_ready = 1;
+		  num = 0;
+	  } break;
+  }
 }
